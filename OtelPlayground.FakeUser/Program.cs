49790eadc1f3824e2310d16c5c53e24fc73a74d3
Reza Adibi <adbrezaooo@gmail.com>
2024-04-23 16:46:48 +0330

    f

---
+++ OtelPlayground.FakeUser/Program.cs
@@ -0,0 +1,25 @@
+﻿{
+    // See https://aka.ms/new-console-template for more information
+    await Task.WhenAll( GetAll());
+    return;
+}
+
+static async Task GetAll()
+{
+    while (true)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(0.5));
+        var response = await new HttpClient().GetAsync($"http://service-a/WeatherForecast/GetAllWeatherForecasts");
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+}
+
+static async Task GetRandomCity()
+{
+    while (true)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(0.5));
+        var response = await new HttpClient().GetAsync($"http://service-a/WeatherForecast/GetAllWeatherForecasts");
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+}
