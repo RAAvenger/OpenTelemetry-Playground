b520ef830baf1bbaffe9a2cfc1d9c779a9931ede
Reza Adibi <adbrezaooo@gmail.com>
2024-04-23 16:48:49 +0330

    f

---
+++ OtelPlayground.ServiceA/Controllers/Logs/LogWhetherForecastExceptions.cs
@@ -0,0 +1,8 @@
+﻿namespace OtelPlayground.ServiceA.Controllers.Logs
+{
+    public static partial class LogWhetherForecastExceptions
+    {
+        [LoggerMessage(EventId = 0, Level = LogLevel.Error)]
+        public static partial void CouldNotGenerateWhetherForecast(this ILogger logger, Exception ex);
+    }
+}
